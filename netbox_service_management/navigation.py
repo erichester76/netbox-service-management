e05236cf95e327ea572b9ead2e15a58d36d97ebd
Eric Hester <eric.hester@umbrella.tech>
2024-10-07 11:53:09 -0400

    fixed button definitions

--- netbox_service_management/navigation.py
+++ netbox_service_management/navigation.py
@@ -5,37 +5,45 @@ items = (
     PluginMenuItem(
         link="plugins:netbox_service_management:solutions_list",
         link_text="Solutions",
-        buttons=PluginMenuButton(
-            link="plugins:netbox_service_management:solutions_add",
-            title="Add",
-            icon_class="mdi mdi-plus-thick",
+        buttons=(
+            PluginMenuButton(
+                link="plugins:netbox_service_management:solutions_add",
+                title="Add",
+                icon_class="mdi mdi-plus-thick",
+            )
         )   
     ),
     PluginMenuItem(
         link="plugins:netbox_service_management:service_list",
         link_text="Service Templates",
-        buttons=PluginMenuButton(
-            link="plugins:netbox_service_management:service_add",
-            title="Add",
-            icon_class="mdi mdi-plus-thick",
-        )   
+        buttons=( 
+            PluginMenuButton(
+                link="plugins:netbox_service_management:service_add",
+                title="Add",
+                icon_class="mdi mdi-plus-thick",
+            )
+        )
     ),  
     PluginMenuItem(
         link="plugins:netbox_service_management:service_list",
         link_text="Services",
-        buttons=PluginMenuButton(
-            link="plugins:netbox_service_management:service_add",
-            title="Add",
-            icon_class="mdi mdi-plus-thick",
+        buttons=(
+            PluginMenuButton(
+                link="plugins:netbox_service_management:service_add",
+                title="Add",
+                icon_class="mdi mdi-plus-thick",
+            )
         )   
     ),  
     PluginMenuItem(
         link="plugins:netbox_service_management:component_list",
         link_text="Components",
-        buttons=PluginMenuButton(
-            link="plugins:netbox_service_management:component_add",
-            title="Add",
-            icon_class="mdi mdi-plus-thick",
+        buttons=(
+            PluginMenuButton(
+                link="plugins:netbox_service_management:component_add",
+                title="Add",
+                icon_class="mdi mdi-plus-thick",
+            )
         )   
     ),
     
