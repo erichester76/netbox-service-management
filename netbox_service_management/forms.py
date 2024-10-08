f6e9f25fc82b13d1bccbdde4f9ca858bcbdd5d9e
Eric Hester <eric.hester@umbrella.tech>
2024-10-08 12:29:07 -0400

    .

--- netbox_service_management/forms.py
+++ netbox_service_management/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.db import models
+from django.db.models import Q
 from ipam.models import Prefix
 from netbox.forms import NetBoxModelForm, NetBoxModelFilterSetForm
 from utilities.forms.fields import CommentField, DynamicModelChoiceField
