3c2b2ec879daef69c1972e841716211282bd7697
Eric Hester <eric.hester@umbrella.tech>
2024-10-08 15:56:53 -0400

    .

--- netbox_service_management/base_views.py
+++ netbox_service_management/base_views.py
@@ -174,36 +174,28 @@ class BaseDetailView(generic.ObjectView):
                             if (related_label, label) not in processed_relationships:
                                 add_node(related_obj, label, current_depth + 1)
 
-            """ # Handle the specific relationship from Component to Service
+             # Handle the specific relationship from Component to Service
             if isinstance(obj, Component) and obj.service:
                 service = obj.service
                 service_label = f"{sanitize_label(service._meta.model_name)}_{service.pk}"
                 
                 # Add the explicit link from Component to Service
                 if (label, service_label) not in processed_relationships:
-                    diagram += f"{label} --> {service_label}\n"
+                    diagram += f"{label} <-- {service_label}\n"
                     processed_relationships.add((label, service_label))
                 
                 # Continue processing relationships for Service, preserving its other connections
                 add_node(service, label, current_depth)
 
-                # Link the Component back to its ServiceTemplateGroupComponent
-                if obj.template_component:
-                    stgc = obj.template_component
-                    stgc_label = f"{sanitize_label(stgc._meta.model_name)}_{stgc.pk}"
-                    if (label, stgc_label) not in processed_relationships:
-                        diagram += f"{label} --> {stgc_label}\n"
-                        processed_relationships.add((label, stgc_label))
-                        add_node(stgc, label, current_depth)
 
             # Directly link the Service to its ServiceTemplate
             if isinstance(obj, Service) and obj.service_template:
                 service_template = obj.service_template
                 st_label = f"{sanitize_label(service_template._meta.model_name)}_{service_template.pk}"
                 if (label, st_label) not in processed_relationships:
-                    diagram += f"{label} --> {st_label}\n"
+                    diagram += f"{label} <-- {st_label}\n"
                     processed_relationships.add((label, st_label))
-                    add_node(service_template, label, current_depth) """
+                    add_node(service_template, label, current_depth)
 
                                 
         # Start the diagram with the main object
