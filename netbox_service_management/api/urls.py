0ab3a46241b63d6fe4348e331d2c5458f2120aca
Eric Hester <eric.hester@umbrella.tech>
2024-10-08 13:14:39 -0400

    .

--- netbox_service_management/api/urls.py
+++ netbox_service_management/api/urls.py
@@ -10,6 +10,8 @@ router.register(r'service_templates', views.ServiceTemplateViewSet)
 router.register(r'service_template_groups', views.ServiceTemplateGroupViewSet)
 router.register(r'service_template_group_components', views.ServiceTemplateGroupComponentViewSet)  
 router.register(r'components', views.ComponentViewSet)
-path('objects/<int:content_type_id>/', views.dynamic_object_list, name='dynamic_object_list'),
 
-urlpatterns = router.urls
+# Add the custom path for the dynamic object list
+urlpatterns = router.urls + [
+    path('objects/<int:content_type_id>/', views.dynamic_object_list, name='dynamic_object_list'),
+]
