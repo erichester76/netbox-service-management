30ddef00061158deae08caf7f13f1a60920d2c0f
Eric Hester <eric.hester@umbrella.tech>
2024-10-08 12:15:47 -0400

    dynamic object linking for component

--- netbox_service_management/forms.py
+++ netbox_service_management/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from ipam.models import Prefix
 from netbox.forms import NetBoxModelForm, NetBoxModelFilterSetForm
 from utilities.forms.fields import CommentField, DynamicModelChoiceField
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 from . import models
 
@@ -25,11 +27,41 @@ class ServiceTemplateGroupComponentForm(NetBoxModelForm):
         model = models.ServiceTemplateGroupComponent
         fields = ("name", "service_template_group", "tags")
         
-class ComponentForm(NetBoxModelForm):
+class ComponentForm(forms.ModelForm):
+    content_type = forms.ModelChoiceField(
+        queryset=ContentType.objects.filter(
+            models.Q(app_label='dcim') | models.Q(app_label='ipam')
+        ),
+        label="Object Type",
+        required=True,
+    )
+    object_id = forms.ChoiceField(label="Object", required=True)
+
     class Meta:
         model = models.Component
-        fields = ("name", "service", "template_component", "tags")                        
+        fields = ['name', 'service', 'template_component', 'tenant', 'content_type', 'object_id']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # Dynamically populate the object_id choices based on the selected content_type
+        if 'content_type' in self.data:
+            try:
+                content_type = ContentType.objects.get(pk=int(self.data.get('content_type')))
+                self.fields['object_id'].choices = self.get_object_choices(content_type)
+            except (ValueError, ContentType.DoesNotExist):
+                self.fields['object_id'].choices = []
+        elif self.instance.pk and self.instance.content_type:
+            content_type = self.instance.content_type
+            self.fields['object_id'].choices = self.get_object_choices(content_type)
+        else:
+            self.fields['object_id'].choices = []
+
+    def get_object_choices(self, content_type):
+        """Return a list of tuples representing the objects of the selected content type."""
+        model_class = content_type.model_class()
+        return [(obj.pk, str(obj)) for obj in model_class.objects.all()]
+    
 class ServiceForm(NetBoxModelForm):
     class Meta:
         model = models.Service
