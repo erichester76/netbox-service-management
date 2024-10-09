42e7bad0f2d399cec4c1282ac039beb68e09d182
Eric Hester <eric.hester@umbrella.tech>
2024-10-09 16:33:38 -0400

    .

--- netbox_service_management/base_views.py
+++ netbox_service_management/base_views.py
@@ -224,7 +224,7 @@ class BaseDetailView(generic.ObjectView):
                 if (label and parent_label) and ('cluster' in parent_label and 'device' in label):
                     diagram += f"%% RETURN-cluster PARENT {parent_label} CHILD:{label} depth {current_depth}\n"
                     return
-                if (label and parent_label) and ('servicetemplategroup' in parent_label and 'service' in parent_label and 'servicetemplate' in label):
+                if (label and parent_label) and ('servicetemplategroup' in parent_label or 'service' in parent_label and 'servicetemplate' in label):
                     diagram += f"%% RETURN-STG-LOOP PARENT {parent_label} CHILD {label} depth {current_depth}\n"
                     return
                 if (label and parent_label) and ('component' in parent_label and 'service' in label):
