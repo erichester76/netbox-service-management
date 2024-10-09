8f22565f4c7adc5a9f860a4a65fc0df9f38e912e
Eric Hester <eric.hester@umbrella.tech>
2024-10-09 17:51:15 -0400

    .

--- netbox_service_management/base_views.py
+++ netbox_service_management/base_views.py
@@ -339,7 +339,7 @@ class BaseDetailView(generic.ObjectView):
                 if rel.is_relation and (sanitize_label(obj._meta.model_name.lower()) not in excluded_model_names) and (rel.name not in excluded_fields):
                     related_objects = getattr(obj, rel.get_accessor_name(), None) if rel.auto_created else getattr(obj, rel.name, None)
                     
-                    nonlocal diagram
+                    #nonlocal diagram
                     diagram += f"%% FIELD {rel.name}\n"
                       
                     # Process the related objects if it's a queryset (reverse relationships)
