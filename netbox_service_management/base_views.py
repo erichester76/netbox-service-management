916a975b245c303c115f0861291572b79d814d89
Eric Hester <eric.hester@umbrella.tech>
2024-10-09 15:10:50 -0400

    .

--- netbox_service_management/base_views.py
+++ netbox_service_management/base_views.py
@@ -200,6 +200,7 @@ class BaseDetailView(generic.ObjectView):
                     label = f"{sanitize_label(obj._meta.model_name.lower())}_{obj.pk}"
                     obj_type = obj._meta.model_name.lower()
 
+                nonlocal diagram
                 diagram += f"#{sanitize_label(obj._meta.model_name.lower())}"
                 diagram += f"#{parent_label}"
                 # Check if we've already visited this node with its relationships processed.
