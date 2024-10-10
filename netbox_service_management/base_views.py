33d30596de1edc921e93a457a77d71c31b40bb68
Eric Hester <eric.hester@umbrella.tech>
2024-10-10 08:19:48 -0400

    .

--- netbox_service_management/base_views.py
+++ netbox_service_management/base_views.py
@@ -272,7 +272,7 @@ class BaseDetailView(generic.ObjectView):
             Adds the start of a subgraph with a given label and description.
             """
             nonlocal diagram
-            diagram += f"\nsubgraph {label} ['']\n"
+            diagram += f"\nsubgraph {label} [ ]\n"
             diagram += f"direction LR\n"
 
         def add_subgraph_end(label):
