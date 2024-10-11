31460088e5b5d0fb2ffc76af9ea3dc570f0ca5e7
Eric Hester <eric.hester@umbrella.tech>
2024-10-11 08:08:32 -0400

    .

--- netbox_service_management/urls.py
+++ netbox_service_management/urls.py
@@ -11,6 +11,7 @@ urlpatterns = (
     path("solutions/<int:pk>/edit/", views.SolutionEditView.as_view(), name="solution_edit"),
     path("solutions/<int:pk>/delete/", views.SolutionDeleteView.as_view(), name="solution_delete"),
     path("solutions/<int:pk>/changelog/", ObjectChangeLogView.as_view(),name="solution_changelog", kwargs={"model": models.Solution}),
+    
     path("service_templates/", views.ServiceTemplateListView.as_view(), name="servicetemplate_list"),
     path("service_templates/add/", views.ServiceTemplateEditView.as_view(), name="servicetemplate_add"),
     path("service_templates/<int:pk>/", views.ServiceTemplateDetailView.as_view(), name="servicetemplate"),
@@ -28,9 +29,9 @@ urlpatterns = (
     path("service_template_group_components/", views.ServiceTemplateGroupComponentListView.as_view(), name="servicetemplategroupcomponent_list"),
     path("service_template_group_components/add/", views.ServiceTemplateGroupComponentEditView.as_view(), name="servicetemplategroupcomponent_add"),
     path("service_template_group_components/<int:pk>/", views.ServiceTemplateGroupComponentDetailView.as_view(), name="servicetemplategroupcomponent"),
-    path("service_template_groups/<int:pk>/edit/", views.ServiceTemplateGroupComponentEditView.as_view(), name="servicetemplategroupcomponent_edit"),
-    path("service_template_groups/<int:pk>/delete/", views.ServiceTemplateGroupComponentDeleteView.as_view(), name="servicetemplategroupcomponent_delete"),
-    path("service_template_groups/<int:pk>/changelog/", ObjectChangeLogView.as_view(), name="servicetemplategroupcomponent_changelog", kwargs={"model": models.ServiceTemplateGroupComponent}),
+    path("service_template_group_components/<int:pk>/edit/", views.ServiceTemplateGroupComponentEditView.as_view(), name="servicetemplategroupcomponent_edit"),
+    path("service_template_group_components/<int:pk>/delete/", views.ServiceTemplateGroupComponentDeleteView.as_view(), name="servicetemplategroupcomponent_delete"),
+    path("service_template_group_components/<int:pk>/changelog/", ObjectChangeLogView.as_view(), name="servicetemplategroupcomponent_changelog", kwargs={"model": models.ServiceTemplateGroupComponent}),
     
     path("components/", views.ComponentListView.as_view(), name="component_list"),
     path("components/add/", views.ComponentEditView.as_view(), name="component_add"),
@@ -49,9 +50,9 @@ urlpatterns = (
     #Bulk Imports
     path('solutions/import/', views.SolutionImportView.as_view(), name='solution_import'),
     path('services/import/', views.ServiceImportView.as_view(), name='service_import'),
-    path('service-templates/import/', views.ServiceTemplateImportView.as_view(), name='servicetemplate_import'),
-    path('service-template-groups/import/', views.ServiceTemplateGroupImportView.as_view(), name='servicetemplategroup_import'),
-    path('service-template-group-components/import/', views.ServiceTemplateGroupComponentImportView.as_view(), name='servicetemplategroupcomponent_import'),
+    path('service_templates/import/', views.ServiceTemplateImportView.as_view(), name='servicetemplate_import'),
+    path('service_template_groups/import/', views.ServiceTemplateGroupImportView.as_view(), name='servicetemplategroup_import'),
+    path('service_template_group_components/import/', views.ServiceTemplateGroupComponentImportView.as_view(), name='servicetemplategroupcomponent_import'),
     path('components/import/', views.ComponentImportView.as_view(), name='component_import'),
 
 )
